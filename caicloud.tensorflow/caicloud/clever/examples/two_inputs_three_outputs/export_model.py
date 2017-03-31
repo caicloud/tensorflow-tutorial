fee1abf7b1c5d16a364ae129cce49bd4a4a9f2b3
lienhua34 <lienhua34@163.com>
2017-03-31 10:49:55 +0800

    样例代码按照 TaaS 2.0 框架进行调整。

--- caicloud.tensorflow/caicloud/clever/examples/two_inputs_three_outputs/export_model.py
+++ caicloud.tensorflow/caicloud/clever/examples/two_inputs_three_outputs/export_model.py
@@ -34,89 +34,67 @@ import os
 import shutil
 import tensorflow as tf
 from caicloud.clever.tensorflow import dist_base
+from caicloud.clever.tensorflow import model_exporter
 
 tf.app.flags.DEFINE_string("export_dir",
                            "/tmp/saved_model/two_inputs_three_output",
                            "模型导出路径。")
 FLAGS = tf.app.flags.FLAGS
 
-class TwoInputsThreeOutputs(dist_base.CaicloudDistTensorflowBase):
-  def build_model(self, global_step, is_chief, sync, num_replicas):
-    #####################
-    # 构建模型
-    # ###################
-    a1 = tf.Variable(0.5, name="a1")
-    b1 = tf.Variable(2.0, name="b1")
-    a2 = tf.Variable(2.0, name="a2")
-    b2 = tf.Variable(3.0, name="b2")
-    a3 = tf.Variable(4.0, name="a3")
-    b3 = tf.Variable(5.0, name="b3")
+def model_fn(sync, num_replicas):
+  #####################
+  # 构建模型
+  # ###################
+  a1 = tf.Variable(0.5, name="a1")
+  b1 = tf.Variable(2.0, name="b1")
+  a2 = tf.Variable(2.0, name="a2")
+  b2 = tf.Variable(3.0, name="b2")
+  a3 = tf.Variable(4.0, name="a3")
+  b3 = tf.Variable(5.0, name="b3")
 
-    # y1 = 0.5*x1 + 2
-    x1 = tf.placeholder(tf.float32, name="x1")
-    y1 = tf.add(tf.mul(a1, x1), b1, name="y1")
+  # y1 = 0.5*x1 + 2
+  x1 = tf.placeholder(tf.float32, name="x1")
+  y1 = tf.add(tf.multiply(a1, x1), b1, name="y1")
 
-    # y2 = 2*x1 + 3
-    y2 = tf.add(tf.mul(a2, x1), b2, name="y2")
+  # y2 = 2*x1 + 3
+  y2 = tf.add(tf.multiply(a2, x1), b2, name="y2")
+  
+  # y1 = 4*x1 + 5
+  x2 = tf.placeholder(tf.float32, name="x2")
+  y3 = tf.add(tf.multiply(a3, x2), b3, name="y3")
 
-    # y1 = 4*x1 + 5
-    x2 = tf.placeholder(tf.float32, name="x2")
-    y3 = tf.add(tf.mul(a3, x2), b3, name="y3")
+  global_step = tf.Variable(0, name='global_step', trainable=False)
 
-    #####################
-    # 初始化模型导出器
-    #####################
-    # Args:
-    #   export_dir: 模型导出路径。
-    #   input_tensors: 导出模型的输入的别名和 Tensors 之间的字典，
-    #       本样例为{'x1': x1, 'x2': x2}。
-    #   output_tensors: 导出模型的输出的别名和 Tensors 之间的字典，
-    #       本样例为{'y1': y1, 'y2': y2, 'y3': y3｝。
-    #   assets_collection: 附加资产文件列表，可选。
-    #   legacy_init_op: 在导出模型被加载要被执行的初始化操作，可选。
-    #   main_op: 导出模型在被加载时执行的操作，可选。
-    self._export_dir = FLAGS.export_dir
-    print("Initialize model exporter, export path:{0}".format(self._export_dir))
-    if os.path.exists(self._export_dir):
-      print("The export path has existed, try to delete it...")
-      shutil.rmtree(self._export_dir)
-      print("The export path has been deleted.")
-    input_tensors = {
-      'x1': x1,
-      'x2': x2,
-    }
-    output_tensors = {
-      'y1': y1,
-      'y2': y2,
-      'y3': y3,
-    }
-    self.init_model_exporter(
-      self._export_dir,
-      input_tensors,
-      output_tensors)
+  # 定义模型导出配置
+  if os.path.exists(FLAGS.export_dir):
+    print("The export path has existed, try to delete it...")
+    shutil.rmtree(FLAGS.export_dir)
+    print("The export path has been deleted.")
+  input_tensors = {
+    'x1': x1,
+    'x2': x2,
+  }
+  output_tensors = {
+    'y1': y1,
+    'y2': y2,
+    'y3': y3,
+  }
+  model_export_spec = model_exporter.ModelExportSpec(
+    export_dir=FLAGS.export_dir,
+    input_tensors=input_tensors,
+    output_tensors=output_tensors)
 
-  def get_init_fn(self, checkpoint_path):
-    """自定义模型初始化函数。
-    """
-    return None
+  return dist_base.ModelFnHandler(
+    global_step=global_step,
+    model_export_spec=model_export_spec)
 
-  def train(self, session, global_step, is_chief):
-    """训练模型
-    """
-    # 该线性模型训练时啥也不做。
-    return True
-
-  def after_train(self, session, is_chief):
-    """模型训练后处理。
-    """
-    #######################
-    ## 模型导出
-    #######################
-    print("Exporting model at {0}".format(self._export_dir))
-    self.export_model(session)
-    print("Export model successfully.")
+def train_fn(session, global_step):
+  """训练模型
+  """
+  # 该线性模型训练时啥也不做。
+  return True
 
 
 if __name__ == "__main__":
-  from caicloud.clever.tensorflow import entry as caicloud_entry
-  caicloud_entry.start(TwoInputsThreeOutputs)
+  distTfRunner = dist_base.DistTensorflowRunner(model_fn = model_fn)
+  distTfRunner.run(train_fn)
